437d0544262fa20b953eb7350127be913674b86e
TennoWarfov <manasvvs.01@mail.ru>
2025-11-10 19:29:07 +0500

    bootstrap, sceneLoader

---
+++ Assets/Modules/Bootstrap/Bootstrap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Modules.SceneController;
+using UnityEngine;
+using Zenject;
+
+namespace Modules.Bootstrap
+{
+    public class Bootstrap : MonoBehaviour
+    {
+        [Inject]
+        private SceneLoader _sceneLoader;
+
+        private async void Start()
+        {
+            try
+            {
+                await _sceneLoader.LoadSceneAsync("Main");
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.ToString()); // TODO handle exception
+            }
+        }
+    }
+}
---
+++ Assets/Modules/SceneController/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Modules.SceneController
+{
+    public class SceneLoader
+    {
+        public async UniTask LoadSceneAsync(string sceneName)
+        {
+            await SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+}
